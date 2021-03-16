f962e512afee056ca53c222959ac7c73bb0d2de8
estraier <hirarin@gmail.com>
2021-03-17 08:50:19 +0900

    Treat SKMAP as optional

--- package_epub.sh
+++ package_epub.sh
@@ -10,6 +10,9 @@ rm -f "${output}"
 pushd "${input}"
 zip -X0 "../${output}" mimetype
 zip -0 "../${output}" META-INF/container.xml OEBPS/package.opf
-zip -0 "../${output}" OEBPS/skmap.xml OEBPS/style.css OEBPS/nav.xhtml
+if [ -f OEBPS/skmap.xml ] ; then
+  zip -0 "../${output}" OEBPS/skmap.xml
+fi
+zip -0 "../${output}" OEBPS/style.css OEBPS/nav.xhtml
 zip -9 "../${output}" OEBPS/overview.xhtml $(ls OEBPS/main-*.xhtml | sort)
 popd
