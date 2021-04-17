e1395e7b073560a7dab9b941c4b1736dacb977a6
estraier <hirarin@gmail.com>
2021-04-17 19:35:27 +0900

    Not update the local config storage unnecessarily

--- chrome-union-dict/browser_action.js
+++ chrome-union-dict/browser_action.js
@@ -4,15 +4,13 @@ let config_form = document.getElementById("config_form");
 
 function update_config(notify) {
   chrome.storage.local.set({"popup_enable": config_form.popup_enable.value}, function(value) {});
-  if (notify) {
-    chrome.runtime.getBackgroundPage(function(background_page) {
-      background_page.update_config();
-    });
-  }
+  chrome.runtime.getBackgroundPage(function(background_page) {
+    background_page.update_config();
+  });
 }
 
 config_form.addEventListener("change", function(event) {
-  update_config(true);
+  update_config();
 });
 
 chrome.storage.local.get(["popup_enable"], function(value) {
@@ -21,5 +19,4 @@ chrome.storage.local.get(["popup_enable"], function(value) {
   } else {
     config_form.popup_enable.value = "on";
   }
-  update_config(false);
 });
