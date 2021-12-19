248da4c1fa9773f8aae68b955a25161e06b119fb
Mikio Hirabayashi <hirarin@gmail.com>
2021-12-20 00:47:57 +0900

    Add a scirpt to make translations from the gross file.

---
+++ make_wiktionary_tran_jaen.py
@@ -0,0 +1,48 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+#--------------------------------------------------------------------------------------------------
+# Script to parse English Wiktionary XML stream and export word information
+#
+# Usage:
+#   make_wiktionary_tran_jaen.py
+#
+# Example:
+#   $ cat wiktionary-gross-jaen.tsv |
+#     ./make_wiktionary_tran_jaen.py > wiktionary-tran-jaen.tsv
+#
+# Copyright 2020 Google LLC
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+# except in compliance with the License.  You may obtain a copy of the License at
+#     https://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the
+# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied.  See the License for the specific language governing permissions
+# and limitations under the License.
+#--------------------------------------------------------------------------------------------------
+
+import collections
+import regex
+import sys
+import tkrzw_tokenizer
+
+tokenizer = tkrzw_tokenizer.Tokenizer()
+word_dict = collections.defaultdict(list)
+for line in sys.stdin:
+  fields = line.strip().split("\t")
+  if len(fields) != 3: continue
+  word, pos, text = fields
+  word = tokenizer.NormalizeJaWordForPos(pos, word)
+  for tran in regex.split(r"[,;]", text):
+    tran = tran.strip()
+    if pos == "alternative": continue
+    if pos == "verb":
+      tran = regex.sub(r"^to ", "", tran)
+    if pos == "noun":
+      tran = regex.sub(r"^(a|an|the) ", "", tran)
+    if not regex.fullmatch(r"[-_\p{Latin}0-9'. ]+", tran): continue
+    tokens = tran.strip(" ")
+    if len(tokens) < 1 or len(tokens) > 5: continue
+    word_dict[tran].append(word)
+
+for word, trans in word_dict.items():
+  print("{}\t{}".format(word, "\t".join(set(trans))))
