c618ec8b3779fc14519c4288c88663af837d055c
Mikio Hirabayashi <hirarin@gmail.com>
2024-01-02 23:03:58 +0900

    Send context tokens.

--- chrome-union-dict-v2/union_dict_pane.js
+++ chrome-union-dict-v2/union_dict_pane.js
@@ -33,6 +33,17 @@ function union_dict_mouseup(event) {
   union_dict_toggle_popup(true);
 }
 
+function tokenize_text(text) {
+  let tokens = [];
+  for (let token of text.split(/\s/)) {
+    token = token.trim();
+    if (token.length > 0) {
+      tokens.push(token);
+    }
+  }
+  return tokens;
+}
+
 function union_dict_toggle_popup(dom_check) {
   if (!document.has_union_dict) {
     if (document.location.href.startsWith(union_dict_search_url)) {
@@ -100,6 +111,49 @@ function union_dict_toggle_popup(dom_check) {
   if (ja_text.length > 48) {
     return;
   }
+  let context_core = "";
+  let context_prefix = "";
+  let context_suffix = "";
+  if (text.length < 48 && ja_text.length < 1 && range.startContainer == range.endContainer) {
+    let whole_text = range.startContainer.textContent;
+    let start = range.startOffset;
+    let end = range.endOffset;
+    if (whole_text.charAt(start).match(/[-A-Za-z0-9]/)) {
+      while (start > 0 && whole_text.charAt(start - 1).match(/[-A-Za-z0-9']/)) {
+        if (start < range.startOffset - 50) break;
+        start--;
+      }
+    }
+    if (whole_text.charAt(end - 1).match(/[-A-Za-z0-9]/)) {
+      while (end < whole_text.length - 1 && whole_text.charAt(end).match(/[-A-Za-z0-9']/)) {
+        if (end > range.endOffset + 50) break;
+        end++;
+      }
+    }
+    context_core = whole_text.substring(start, end);
+    let seg_start = start;
+    while (seg_start > 0 &&
+           whole_text.charAt(seg_start - 1).match(/[-A-Za-z0-9' ]/)) {
+      if (seg_start < start - 100) break;
+      seg_start--;
+    }
+    let seg_end = end;
+    while (seg_end < whole_text.length &&
+           whole_text.charAt(seg_end).match(/[-A-Za-z0-9' ]/)) {
+      if (seg_end > end + 100) break;
+      seg_end++;
+    }
+    let prefix = whole_text.substring(seg_start, start).trim();
+    let suffix = whole_text.substring(end, seg_end).trim();
+    let prefix_tokens = tokenize_text(prefix);
+    let suffix_tokens = tokenize_text(suffix);
+    let num_affix_tokens = 3;
+    prefix_tokens = prefix_tokens.slice(Math.max(0, prefix_tokens.length - num_affix_tokens),
+                                                 prefix_tokens.length);
+    suffix_tokens = suffix_tokens.slice(0, Math.min(num_affix_tokens, suffix_tokens.length));
+    context_prefix = prefix_tokens.join(" ");
+    context_suffix = suffix_tokens.join(" ");
+  }
   union_dict_pane.style.display = "block";
   let pane_left = Math.min(rect.left, window.innerWidth - union_dict_pane.offsetWidth - 8);
   let pane_top = rect.top + rect.height + 5;
@@ -117,19 +171,31 @@ function union_dict_toggle_popup(dom_check) {
   pane_top += window.pageYOffset;
   union_dict_pane.style.left = pane_left + "px";
   union_dict_pane.style.top = pane_top + "px";
-  union_dict_update_pane(text);
+  union_dict_update_pane(text, context_core, context_prefix, context_suffix);
 }
 
-function union_dict_update_pane(query) {
-  if (query == union_dict_pane.last_query) {
+function union_dict_update_pane(text, context_core, context_prefix, context_suffix) {
+  let joined_query = text + ":" + context_core + ":" + context_prefix + ":" + context_suffix;
+  if (joined_query == union_dict_pane.last_query) {
     return;
   }
+  union_dict_pane.last_query = joined_query;
   while (union_dict_pane.firstChild) {
     union_dict_pane.removeChild(union_dict_pane.firstChild);
   }
+  let search_url = union_dict_search_url + "?x=popup&q=" + encodeURIComponent(text);
+  if (context_core.length > 0 && context_core != text) {
+    search_url += "&xc=" + encodeURIComponent(context_core);
+  }
+  if (context_prefix.length > 0) {
+    search_url += "&xp=" + encodeURIComponent(context_prefix);
+  }
+  if (context_suffix.length > 0) {
+    search_url += "&xs=" + encodeURIComponent(context_suffix);
+  }
   let sub_pane = document.createElement("iframe");
   sub_pane.id = "union_dict_sub_pane";
-  sub_pane.src = union_dict_search_url + "?x=popup&q=" + encodeURIComponent(query);
+  sub_pane.src = search_url;
   union_dict_pane.appendChild(sub_pane);
   return;
 }
